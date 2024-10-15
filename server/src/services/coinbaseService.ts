9a062d2418d1d6edfe49e74b62d421dc8a54bcac
rahul.inmenzo <rahul@inmenzo.com>
2024-10-15 23:02:40 +0530

    changes

--- server/src/services/coinbaseService.ts
+++ server/src/services/coinbaseService.ts
@@ -1,8 +1,9 @@
 // src/services/coinbaseService.ts
 import WebSocket from 'ws';
 import { distributeMessage } from './websocketService';
-
-
+import dotenv from 'dotenv';
+dotenv.config();
+console.log("COINBASE_WS_UR : " + process.env.COINBASE_WS_UR);
 const coinbaseWs = new WebSocket(process.env.COINBASE_WS_URL);
 
 coinbaseWs.on('message', (data: WebSocket.Data) => {
